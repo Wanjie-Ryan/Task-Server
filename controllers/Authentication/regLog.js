07b9cb4231f653021486e5cc6007ec9c3a8c26a0
Ryan <ryanwanjiedistortion@gmail.com>
2023-08-29 14:15:24 +0300

    task-server

--- controllers/Authentication/regLog.js
+++ controllers/Authentication/regLog.js
@@ -1,106 +1,115 @@
-const AuthModel = require('../../models/RegLog/reglog')
-const {StatusCodes} = require('http-status-codes')
-const jwt = require('jsonwebtoken')
-const bcrypt = require('bcryptjs')
+const AuthModel = require("../../models/RegLog/reglog");
+const { StatusCodes } = require("http-status-codes");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
+const Register = async (req, res) => {
+  try {
+    const { name, email, contact, company, role, password } = req.body;
 
-
-const Register = async(req,res)=>{
-
-
-    try{
-
-        const {name,email,contact,company,role,password} = req.body
-
-        if(!name || !email || !contact || !company || !role || !password){
-
-            return res.status(StatusCodes.BAD_REQUEST).json({msg:'please fill all the fields'})
-        }
-
-        const newUser = await AuthModel.create(req.body)
-
-        return res.status(StatusCodes.CREATED).json({msg:'user created successfully'})
-
-
+    if (!name || !email || !contact || !company || !role || !password) {
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ msg: "please fill all the fields" });
     }
 
-    catch(err){
+    const newUser = await AuthModel.create(req.body);
 
-        // console.log(err)
-        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({msg:'Something went wrong, try again later'})
+    return res
+      .status(StatusCodes.CREATED)
+      .json({ msg: "user created successfully" });
+  } catch (err) {
+    // console.log(err)
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ msg: "Something went wrong, try again later" });
+  }
+};
 
+const Login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
 
+    if (!email || !password) {
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ msg: "Provide all the fields" });
     }
-    
-}
 
+    const UserEmail = await AuthModel.findOne({ email });
 
-const Login = async(req,res)=>{
+    if (!UserEmail) {
+      return res
+        .status(StatusCodes.NOT_FOUND)
+        .json({ msg: "The Email Provided cannot be found" });
+    }
 
-    try{
+    const correctPassword = await UserEmail.checkpwd(password);
 
-        const {email, password} = req.body
+    if (!correctPassword) {
+      return res
+        .status(StatusCodes.UNAUTHORIZED)
+        .json({ msg: "Incorrect password" });
+    }
 
-        if(!email || !password){
+    const UserLogin = UserEmail.toObject();
+    delete UserLogin.password;
+    delete UserLogin.company;
+    delete UserLogin.contact;
+    delete UserLogin.email;
 
-            return res.status(StatusCodes.BAD_REQUEST).json({msg:'Provide all the fields'})
-        }
+    const token = jwt.sign(
+      { userId: UserLogin._id },
+      process.env.user_secret_key,
+      { expiresIn: "1d" }
+    );
 
-        const UserEmail = await AuthModel.findOne({email})
-
-        if(!UserEmail){
-
-            return res.status(StatusCodes.NOT_FOUND).json({msg:'The Email Provided cannot be found'})
-        }
-
-        const correctPassword = await UserEmail.checkpwd(password);
-
-        if (!correctPassword) {
-            return res
-              .status(StatusCodes.UNAUTHORIZED)
-              .json({ msg: "Incorrect password" });
-        }
-
-        const UserLogin = UserEmail.toObject()
-        delete UserLogin.password
-        delete UserLogin.company
-        delete UserLogin.contact
-        delete UserLogin.email
-
-
-        const token = jwt.sign(
-            { userId: UserLogin._id },
-            process.env.user_secret_key,
-            { expiresIn: "1d" }
-          );
-
-          return res
+    return res
       .status(StatusCodes.OK)
       .json({ msg: `Login Successful`, UserLogin, userToken: token });
+  } catch (err) {
+    // console.log(err)
 
-      
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ msg: "Something went wrong, please try again later" });
+  }
+};
 
+const UpdateProfile = async (req, res) => {
+  try {
+    const { photo, password } = req.body;
+    const { id: userid } = req.params;
 
-
+    if (password) {
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(password, salt);
+      req.body.password = hashedPassword;
     }
 
-    catch(err){
+    const updateUser = await AuthModel.findByIdAndUpdate(
+      { _id: userid },
+      req.body,
+      { new: true, runValidators: true }
+    );
 
-        console.log(err)
-
-        res
-        .status(StatusCodes.INTERNAL_SERVER_ERROR)
-        .json({ msg: "Something went wrong, please try again later" });
+    if(!updateUser){
+        return res.status(StatusCodes.NOT_FOUND).json({msg:`The user has not been found`})
     }
- 
+
+    return res.status(StatusCodes.OK).json({msg:'Your Profile has been updated Successfully', updateUser})
+
+  } catch (err) {
+    console.log(err);
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ msg: "Something went wrong, please try again later" });
+  }
+};
+
+const getSingleUser  = (req,res)=>{
+
+    res.send('send')
 }
 
-const UpdateProfile = (req,res)=>{
-
-    res.send('profile')
-}
-
-
-
-
-module.exports = {Register,Login,UpdateProfile}
+module.exports = { Register, Login, UpdateProfile,getSingleUser };
--- routes/Authentication/regLog.js
+++ routes/Authentication/regLog.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const router = express.Router()
-const {Register,Login,UpdateProfile} = require('../../controllers/Authentication/regLog')
-
+const {Register,Login,UpdateProfile,getSingleUser} = require('../../controllers/Authentication/regLog')
+const AuthMiddleware =  require('../../middleware/auth')
 
 router.route('/register').post(Register)
 router.route('/login').post(Login)
-router.route('/updateprofile/:id').patch(UpdateProfile)
-
+router.route('/updateprofile/:id').patch(AuthMiddleware,UpdateProfile)
+router.route('/singleuser/:id').get(getSingleUser)
 
 
 module.exports = router
