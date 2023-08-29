ac21ef09f4af115a32ba7e3b31b0cae09b7e46e8
Ryan <ryanwanjiedistortion@gmail.com>
2023-08-29 12:36:31 +0300

    task-server

--- controllers/Authentication/regLog.js
+++ controllers/Authentication/regLog.js
@@ -19,15 +19,15 @@ const Register = async(req,res)=>{
 
         const newUser = await AuthModel.create(req.body)
 
-        return res.status(StatusCodes.CREATED).json({msg:'user created successfully', newUser})
+        return res.status(StatusCodes.CREATED).json({msg:'user created successfully'})
 
 
     }
 
     catch(err){
 
-        console.log(err)
-        res.status(StatusCodes.INTRNAL_SERVER_ERROR).json({msg:'Something went wrong, try again later'})
+        // console.log(err)
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({msg:'Something went wrong, try again later'})
 
 
     }
@@ -35,9 +35,69 @@ const Register = async(req,res)=>{
 }
 
 
-const Login =(req,res)=>{
+const Login = async(req,res)=>{
 
-    res.send('login')
+    try{
+
+        const {email, password} = req.body
+
+        if(!email || !password){
+
+            return res.status(StatusCodes.BAD_REQUEST).json({msg:'Provide all the fields'})
+        }
+
+        const UserEmail = await AuthModel.findOne({email})
+
+        if(!UserEmail){
+
+            return res.status(StatusCodes.NOT_FOUND).json({msg:'The Email Provided cannot be found'})
+        }
+
+        const correctPassword = await UserEmail.checkpwd(password);
+
+        if (!correctPassword) {
+            return res
+              .status(StatusCodes.UNAUTHORIZED)
+              .json({ msg: "Incorrect password" });
+        }
+
+        const UserLogin = UserEmail.toObject()
+        delete UserLogin.password
+        delete UserLogin.company
+        delete UserLogin.contact
+        delete UserLogin.email
+
+
+        const token = jwt.sign(
+            { userId: UserLogin._id },
+            process.env.user_secret_key,
+            { expiresIn: "1d" }
+          );
+
+          return res
+      .status(StatusCodes.OK)
+      .json({ msg: `Login Successful`, UserLogin, userToken: token });
+
+      
+
+
+
+    }
+
+    catch(err){
+
+        console.log(err)
+
+        res
+        .status(StatusCodes.INTERNAL_SERVER_ERROR)
+        .json({ msg: "Something went wrong, please try again later" });
+    }
+ 
+}
+
+const UpdateProfile = (req,res)=>{
+
+    res.send('profile')
 }
 
 
