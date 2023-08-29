644bc6a70d8c69fefe40d555e7858840975ef8f9
Ryan <ryanwanjiedistortion@gmail.com>
2023-08-29 19:07:20 +0300

    task-server

--- models/Admin/projects.js
+++ models/Admin/projects.js
@@ -1,40 +1,36 @@
 const mongoose = require("mongoose");
 
-
-const projectSchema = new mongoose.Schema({
-
-    createdBy:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:'User',
-        required:true
+const projectSchema = new mongoose.Schema(
+  {
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
     },
 
-    name:{
-        type:String,
-        required:[true,'name of project must be provided']
+    name: {
+      type: String,
+      required: [true, "name of project must be provided"],
     },
 
-    deadline:{
-        type:Date,
-        required:[true, 'deadline date must be provided']
-
+    deadline: {
+      type: Date,
+      required: [true, "deadline date must be provided"],
     },
 
-    description:{
-        type:String,
-        required:[true, 'project description must be provided']
+    description: {
+      type: String,
+      required: [true, "project description must be provided"],
     },
 
     status: {
-        type: String,
-        enum: ["pending", "complete"],
-        default: "Pending",
-        required: [true, "Status of Project is required"],
-      },
+      type: String,
+      enum: ["pending", "complete"],
+      default: "Pending",
+      required: [true, "Status of Project is required"],
+    },
+  },
+  { timestamps: true }
+);
 
-
-}, {timestamps:true})
-
-
-
-module.exports = mongoose.model('Projects', projectSchema)
+module.exports = mongoose.model("Projects", projectSchema);
