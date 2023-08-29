1d199bddecd1e0766dc51ca06eb53da06677aad0
Ryan <ryanwanjiedistortion@gmail.com>
2023-08-29 09:49:14 +0300

    task-server

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3005
+const cors = require("cors");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const cookie = require("cookie-parser");
+const rateLimit = require("express-rate-limit");
+require("dotenv").config();
+const { StatusCodes } = require("http-status-codes");
+const connectionDB = require('./connection/connection')
+
+
+
+app.use(helmet());
+app.use(xss());
+app.use(cookie());
+app.use(express.json());
+app.set("trust proxy", 1);
+app.use(
+  rateLimit({
+    windowsMs: 15 * 60 * 100,
+    max: 100,
+  })
+);
+app.use(cors());
+
+
+
+
+const DBConnection = async () => {
+    try {
+      await connectionDB(process.env.mongo_url);
+  
+      app.listen(port, () => {
+        console.log(`server is running on port, ${port}`);
+      });
+    } catch (err) {
+      console.log(err);
+      process.exit(1);
+    }
+  };
+  
+  DBConnection();
