a1907d97c5e30a1dab6974ab0f0da901e4521e42
Ryan <ryanwanjiedistortion@gmail.com>
2023-08-30 16:18:38 +0300

    task-server

--- routes/Authentication/regLog.js
+++ routes/Authentication/regLog.js
@@ -15,6 +15,6 @@ router.route("/login").post(Login);
 router.route("/updateprofile/:id").patch(AuthMiddleware, UpdateProfile);
 router.route("/singleuser/:id").get(AuthMiddleware, getSingleUser);
 router.route('/userrole').get(AuthMiddleware,GetUserRole)
-router.route("/verify").get(verifyToken);
+router.route("/verify").get(AuthMiddleware,verifyToken);
 
 module.exports = router;
