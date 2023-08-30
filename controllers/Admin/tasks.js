0c28d7671f7f24d827033e75d2a33406b8b37079
Ryan <ryanwanjiedistortion@gmail.com>
2023-08-30 10:16:21 +0300

    task-server

--- controllers/Admin/tasks.js
+++ controllers/Admin/tasks.js
@@ -1,15 +1,24 @@
-const UserModel = require('../../models/RegLog/reglog')
-const ProjectsModel = require('../../models/Admin/projects')
-const taskModel = require('../../models/Admin/tasks')
-const jwt = require('jsonwebtoken')
+const UserModel = require("../../models/RegLog/reglog");
+const ProjectsModel = require("../../models/Admin/projects");
+const taskModel = require("../../models/Admin/tasks");
+const jwt = require("jsonwebtoken");
+const { StatusCodes } = require("http-status-codes");
 
+const CreateTask = async (req, res) => {
+  try {
+    const { project, name, description, deadline, status, assign } = req.body;
 
+    const newTask = await taskModel.create(req.body);
 
+    return res
+      .status(StatusCodes.CREATED)
+      .json({ msg: "The Task was created successfully", newTask });
+  } catch (err) {
+    console.log(err);
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ msg: "Something went wrong, please try again later" });
+  }
+};
 
-const CreateTask = (req,res)=>{
-
-    res.send('hey')
-}
-
-
-module.exports = {CreateTask}
+module.exports = { CreateTask };
