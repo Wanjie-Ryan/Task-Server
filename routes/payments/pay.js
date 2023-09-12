88421f77bf14b47d823e9eeb1f4a18b8cd03e680
Ryan <ryanwanjiedistortion@gmail.com>
2023-09-12 06:24:21 +0300

    payment

--- routes/payments/pay.js
+++ routes/payments/pay.js
@@ -1,11 +1,8 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const {CreatePayment} = require('../../controllers/payments/pay')
+const { CreatePayment } = require("../../controllers/payments/pay");
 const AuthMiddleware = require("../../middleware/auth");
 
+router.route("/payment").post(AuthMiddleware, CreatePayment);
 
-
-router.route('/payment').post(AuthMiddleware,CreatePayment)
-
-
-module.exports = router
+module.exports = router;
