5e6df41971ae121d713c61b550fab437255277ef
Ryan <ryanwanjiedistortion@gmail.com>
2023-09-12 06:21:56 +0300

    payment

--- controllers/payments/pay.js
+++ controllers/payments/pay.js
@@ -1,13 +1,54 @@
-const payment = require('../../models/payments/pay')
+const paymentModel = require('../../models/payments/pay')
 const UserModel = require("../../models/RegLog/reglog");
 const jwt = require("jsonwebtoken");
 const { StatusCodes } = require("http-status-codes");
 
 
 
-const CreatePayment =(req,res)=>{
+const CreatePayment =async(req,res)=>{
 
-    res.send('payment created')
+    try{
+
+        const {transactedBy, status} = req.body
+
+        const token = req.headers.authorization.split(" ")[1];
+    if (!token) {
+        return res
+          .status(StatusCodes.UNAUTHORIZED)
+          .json({ msg: "You are not authorized to create a project" });
+
+      }
+
+      const decodedToken = jwt.verify(token, process.env.user_secret_key);
+    const userId = decodedToken.userId;
+
+    const findOneUser = await UserModel.findById(userId);
+
+    if (!findOneUser) {
+        return res.status(StatusCodes.NOT_FOUND).json({
+          msg: "User does not exist, therefore, cannot create the project",
+        });
+      }
+
+      const newPayment = await paymentModel.create(...req.body)
+
+      return res.status(StatusCodes.OK).json({msg:'Payment has been received successfully', newPayment})
+
+
+
+
+    }
+
+    catch(err){
+
+        // console.log(err)
+
+        res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ msg: "Something went wrong, please try again later" });
+    }
+
+    
 }
 
 
--- routes/payments/pay.js
+++ routes/payments/pay.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const {CreatePayment} = require('../../controllers/payments/pay')
+const AuthMiddleware = require("../../middleware/auth");
 
 
 
-router.route('/payment').post(CreatePayment)
+router.route('/payment').post(AuthMiddleware,CreatePayment)
 
 
 module.exports = router
